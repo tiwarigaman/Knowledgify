3f129f971eedb00707fe4eeb2aa7bab7010261f9
Aman Tiwari <amant6393@gmail.com>
2024-07-16 11:12:06 +0530

    corner cases added

--- app/src/main/java/com/mobile/pdhayi/ApiUrls.kt
+++ app/src/main/java/com/mobile/pdhayi/ApiUrls.kt
@@ -4,4 +4,7 @@ object ApiUrls {
     const val REGISTER = "https://knowledgify-backend.onrender.com/api/register"
     const val LOGIN = "https://knowledgify-backend.onrender.com/api/login"
     const val VERIFY = "https://knowledgify-backend.onrender.com/api/verify"
+    const val BETA_VERIFY = "https://knowledgify-backend-beta.onrender.com/api/verify"
+    const val BETA_GOOGLE_SIGN_UP = "https://knowledgify-backend-1.onrender.com/api/googleSignUp"
+    const val BETA_GOOGLE_SIGN_IN = "https://knowledgify-backend-1.onrender.com/api/googleSignIn"
 }
--- app/src/main/java/com/mobile/pdhayi/SignUpNormal.kt
+++ app/src/main/java/com/mobile/pdhayi/SignUpNormal.kt
@@ -5,14 +5,31 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import org.json.JSONObject
 
 class SignUpNormal : AppCompatActivity() {
+    private lateinit var googleSignInOptions: GoogleSignInOptions
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private val RC_SIGN_IN = 1000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up_normal)
+        googleSignInOptions =
+            GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build()
+        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions)
+
+        findViewById<TextView>(R.id.googleSignInOther).setOnClickListener {
+            signIn()
+        }
 
         val firstName: EditText = findViewById(R.id.firstName)
         val lastName: EditText = findViewById(R.id.lastName)
@@ -50,25 +67,101 @@ class SignUpNormal : AppCompatActivity() {
                 ApiClient.post(ApiUrls.REGISTER, json) { success, code, responseBody ->
                     runOnUiThread {
                         if (success) {
-                            Toast.makeText(this@SignUpNormal, responseBody, Toast.LENGTH_SHORT).show()
-                            val intent = Intent(applicationContext, VerificationActivity::class.java).apply {
-                                putExtra("email", email.text.toString())
-                                putExtra("password", password.text.toString())
-                                putExtra("firstName", firstName.text.toString())
-                                putExtra("lastName", lastName.text.toString())
-                            }
+                            Toast.makeText(this@SignUpNormal, responseBody, Toast.LENGTH_SHORT)
+                                .show()
+                            val intent =
+                                Intent(applicationContext, VerificationActivity::class.java).apply {
+                                    putExtra("email", email.text.toString())
+                                    putExtra("password", password.text.toString())
+                                    putExtra("firstName", firstName.text.toString())
+                                    putExtra("lastName", lastName.text.toString())
+                                }
                             startActivity(intent)
                             finish()
                         } else {
                             Log.d("API ResponseE", code.toString())
                             if (code == 400) {
-                                Toast.makeText(applicationContext, "Email already exists, Please try to login", Toast.LENGTH_SHORT).show()
+                                Toast.makeText(
+                                    applicationContext,
+                                    "Email already exists, Please try to login",
+                                    Toast.LENGTH_SHORT
+                                ).show()
                             } else {
-                                Toast.makeText(applicationContext, "Something went wrong", Toast.LENGTH_SHORT).show()
+                                Toast.makeText(
+                                    applicationContext,
+                                    "Something went wrong",
+                                    Toast.LENGTH_SHORT
+                                ).show()
                             }
                         }
                     }
                 }
+            }
+        }
+    }
+
+    private fun signIn() {
+        val intent = googleSignInClient.signInIntent
+        startActivityForResult(intent, RC_SIGN_IN)
+    }
+
+    @Deprecated("This method has been deprecated in favor of using the Activity Result API\n      which brings increased type safety via an {@link ActivityResultContract} and the prebuilt\n      contracts for common intents available in\n      {@link androidx.activity.result.contract.ActivityResultContracts}, provides hooks for\n      testing, and allow receiving results in separate, testable classes independent from your\n      activity. Use\n      {@link #registerForActivityResult(ActivityResultContract, ActivityResultCallback)}\n      with the appropriate {@link ActivityResultContract} and handling the result in the\n      {@link ActivityResultCallback#onActivityResult(Object) callback}.")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                task.getResult(ApiException::class.java)
+                val account: GoogleSignInAccount =
+                    GoogleSignIn.getSignedInAccountFromIntent(data).result
+                if (account != null) {
+                    Toast.makeText(
+                        applicationContext,
+                        "${account.displayName} ${account.email}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    val firstName: String = account.displayName!!.split(" ")[0]
+                    val lastName: String = account.displayName!!.split(" ")[1]
+                    val json = JSONObject().apply {
+                        put("firstName", firstName)
+                        put("lastName", lastName)
+                        put("email", account.email)
+                    }
+                    ApiClient.post(ApiUrls.BETA_GOOGLE_SIGN_UP, json) { success, code, responseBody ->
+                        runOnUiThread {
+                            if (success) {
+                                Toast.makeText(this@SignUpNormal, responseBody, Toast.LENGTH_SHORT)
+                                    .show()
+                                val intent =
+                                    Intent(applicationContext, HomeActivity::class.java).apply {
+                                        putExtra("email", account.email)
+                                        putExtra("password", "password@123")
+                                        putExtra("firstName", firstName)
+                                        putExtra("lastName", lastName)
+                                    }
+                                startActivity(intent)
+                                finish()
+                            } else {
+                                Log.d("API ResponseE", code.toString())
+                                if (code == 401) {
+                                    Toast.makeText(
+                                        applicationContext,
+                                        "Email already exists, Please try to login",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                } else {
+                                    Toast.makeText(
+                                        applicationContext,
+                                        "Something went wrong",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                }
+                            }
+                        }
+                    }
+                }
+            } catch (e: ApiException){
+                Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
             }
         }
     }
