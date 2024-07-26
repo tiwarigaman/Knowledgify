905f44b82a69851244323912d67f7cb716b56fba
Aman Tiwari <amant6393@gmail.com>
2024-07-26 17:35:44 +0530

    search fragment complete

---
+++ app/src/main/java/com/mobile/pdhayi/data_modal/SuggestionNetworkDataClass.kt
@@ -0,0 +1,7 @@
+package com.mobile.pdhayi.data_modal
+
+class SuggestionNetworkDataClass (
+    val suggestionImage : String,
+    val nameSuggestion : String,
+    val descriptionSuggestion : String
+)
