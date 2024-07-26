0a36eca207a54d83f7fef1b1a4bee1e92f01e862
Aman Tiwari <amant6393@gmail.com>
2024-07-26 13:24:34 +0530

    connection page items container completed

--- app/src/main/java/com/mobile/pdhayi/fragment/SearchFragment.kt
+++ app/src/main/java/com/mobile/pdhayi/fragment/SearchFragment.kt
@@ -16,4 +16,10 @@ class SearchFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_search, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
 }
