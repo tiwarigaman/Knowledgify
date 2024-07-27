b863274bb15b49da3ba047291d8fd0d8fb1feb9c
Aman Tiwari <amant6393@gmail.com>
2024-07-27 22:18:40 +0530

    Add post initiated

---
+++ app/src/main/java/com/mobile/pdhayi/AddPost.kt
@@ -0,0 +1,12 @@
+package com.mobile.pdhayi
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AddPost : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_post)
+
+    }
+}
