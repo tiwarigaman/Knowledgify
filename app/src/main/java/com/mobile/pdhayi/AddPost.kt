1862c81efb71f2ad4ee4e8275106243517ac89f0
Aman Tiwari <amant6393@gmail.com>
2024-07-28 11:59:09 +0530

    lecture UI changed

--- app/src/main/java/com/mobile/pdhayi/AddPost.kt
+++ app/src/main/java/com/mobile/pdhayi/AddPost.kt
@@ -1,12 +1,19 @@
 package com.mobile.pdhayi
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class AddPost : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_post)
+
+        findViewById<ImageView>(R.id.backBtnSearch).setOnClickListener {
+            finish()
+        }
 
     }
 }
--- app/src/main/java/com/mobile/pdhayi/fragment/LectureFragment.kt
+++ app/src/main/java/com/mobile/pdhayi/fragment/LectureFragment.kt
@@ -5,6 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.denzcoskun.imageslider.ImageSlider
+import com.denzcoskun.imageslider.constants.ScaleTypes
+import com.denzcoskun.imageslider.models.SlideModel
 import com.mobile.pdhayi.R
 
 class LectureFragment : Fragment() {
@@ -15,5 +18,21 @@ class LectureFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_lecture, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val imageSlider = view.findViewById<ImageSlider>(R.id.image_slider_lecture)
+
+        val imageList = ArrayList<SlideModel>()
+
+        // Add images to the list
+        imageList.add(SlideModel("https://i.pinimg.com/564x/03/bc/cb/03bccb3fb8d3d8993cef1cda582f6425.jpg"))
+        imageList.add(SlideModel("https://i.pinimg.com/564x/03/bc/cb/03bccb3fb8d3d8993cef1cda582f6425.jpg"))
+        imageList.add(SlideModel("https://i.pinimg.com/564x/fa/43/5c/fa435c8f53e9d502476b411100345ef1.jpg", "Image 2"))
+        imageList.add(SlideModel("https://i.pinimg.com/564x/fa/43/5c/fa435c8f53e9d502476b411100345ef1.jpg", "Image 2"))
+
+        imageSlider.setImageList(imageList, ScaleTypes.CENTER_CROP)
+        imageSlider.startSliding(3000)
     }
 }
